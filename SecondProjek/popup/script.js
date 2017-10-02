a9a67c44a3c7b669114f19f813e9bbc9e28d2778
altmaneddie <altman.eduard@gmail.com>
2017-10-02 12:42:43 +0300

    monday v1

--- SecondProjek/popup/script.js
+++ SecondProjek/popup/script.js
@@ -1,3 +1,4 @@
+// Auto-Add button => add current url to playlist.
 function addEvents() {
     $("#ytBtn").on("click", function () {
         var urlRegex = /^(https?\:\/\/)?(www\.)?(youtube\.com|youtu\.?be)\/.+$/;
@@ -5,8 +6,11 @@ function addEvents() {
         gettingActiveTab.then(function (tabs) {
             tempUrl = tabs[0].url;
             if (urlRegex.test(tempUrl)) {
-                console.log("a")
-                browser.tabs.sendMessage(tabs[0].id, { name: "ytA", link: tempUrl });
+                browser.storage.local.get("ytPL").then((playList) => {
+                    let currPlayList = playList.ytPL;
+                    currPlayList.push(tempUrl);
+                    browser.storage.local.set({ ytPL: currPlayList });
+                })
             } else {
                 alert("You are not on a valid Youtube page");
             }
@@ -14,6 +18,26 @@ function addEvents() {
     })
 }
 
+// Add button => play video if storage has been changed and playlist length is 1 (so it wont repeat itself and no button will appear when to item is in the playlist)
+function playBtn() {
+    browser.storage.local.get().then((url) => {
+        if (Object.keys(url).length !== 0) {
+            if (url.ytPL.length !== 0 && url.ytPL.length < 2) {
+                $("#yt").append($("<input type='button' id='ytPlayBtn' value='Play the Video'>"))
+                $("ytPlayBtn").on("click", function () {
+                    var gettingActiveTab = browser.tabs.query({ active: true, currentWindow: true });
+                    gettingActiveTab.then(function (tabs) {
+                        browser.tabs.sendMessage(tabs[0].id, { name: "playYt", currentUrl: url });
+                    });
+                })
+            }
+        } else {
+            browser.storage.local.set({ ytPL: [] })
+        }
+    })
+}
+
+// Auto Fct => adds all elements to the pop-up
 function popCreator() {
     $("#myExtApp").append($("<div id='yt'></div>"));
     $("#yt").append($("<input type='button' id='ytBtn' value='Get this video!'>"));
@@ -21,20 +45,7 @@ function popCreator() {
     $("#writeS").append($("<input type='button' id='randomBtn' value='I want to write stuff'>"));
 
     addEvents();
-
-    browser.storage.local.get("url").then((url) => {
-        console.log(url);
-        if (url !== undefined) {
-            $("#yt").append($("<input type='button' id='ytPlayBtn' value='Play the Video'>"))
-            $("ytPlayBtn").on("click", function () {
-                var gettingActiveTab = browser.tabs.query({ active: true, currentWindow: true });
-                gettingActiveTab.then(function (tabs) {
-                    browser.tabs.sendMessage(tabs[0].id, { name: "playYt", currentUrl: url });
-                });
-            })
-        }
-    })
-
 }
 
 popCreator();
+browser.storage.onChanged.addListener(playBtn);
