cfdbb088747951fe77794ddb32d6aafb1eb415fc
altmaneddie <altman.eduard@gmail.com>
2017-10-03 16:05:00 +0300

    tuesday v3.0

--- SecondProjek/background/background_scripts.js
+++ SecondProjek/background/background_scripts.js
@@ -1,21 +1,21 @@
-function masterFunction(message, sender, sendResponse) {
-    if (message.hasOwnProperty("name")) {
-        console.log("entered if");
-        browser.storage.local.get("ytPL").then((url) => {
-            if (message.name === "ytA") {
-                console.log("entered second if");
-                url.push(message.link);
-            } else if (message.name === "ytR") {
-                var ind = url.indexOf(message.link)
-                if (ind > -1) {
-                    url.splice(i, 1);
-                    browser.storage.local.set({ ytPL: url });
-                }
-            }
-        }
-        )
-    }
-}
+// function masterFunction(message, sender, sendResponse) {
+//     if (message.hasOwnProperty("name")) {
+//         console.log("entered if");
+//         browser.storage.local.get("ytPL").then((url) => {
+//             if (message.name === "ytA") {
+//                 console.log("entered second if");
+//                 url.push(message.link);
+//             } else if (message.name === "ytR") {
+//                 var ind = url.indexOf(message.link)
+//                 if (ind > -1) {
+//                     url.splice(i, 1);
+//                     browser.storage.local.set({ ytPL: url });
+//                 }
+//             }
+//         }
+//         )
+//     }
+// }
 
 
-browser.runtime.onMessage.addListener(backgroundFunction);
+// browser.runtime.onMessage.addListener(backgroundFunction);
