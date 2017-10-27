1dd9c3d7e6ff45d48dfde719239922cee1b63d17
altmaneddie <altman.eduard@gmail.com>
2017-10-27 11:17:46 +0300

    27.10

--- ThirdProjekt/content-scripts/content.js
+++ ThirdProjekt/content-scripts/content.js
@@ -1,8 +1,18 @@
-function playYt(){
+function onYouTubeApiLoad() {
+    console.log("api received");
+}
+
+function playYt() {
     $("body").first().prepend($("<div id='ytWrapper'></div>"));
     $("#ytWrapper").html(`<iframe id="tehVideo" src="https://www.youtube.com/watch?v=EtRlNd3dYbw" frameborder="0" allowfullscreen></iframe>`);
     $("#ytWrapper").append($('<button type="button" id="randomBtn">Randomize</button>'));
+
+    var tag = document.createElement('script');
+    tag.src = "https://www.youtube.com/iframe_api";
+    onYouTubeApiLoad();
 }
+
+
 
 function masterFunction(message, sender, sendResponse) {
     if (message.name === "playYt") {
@@ -11,3 +21,6 @@ function masterFunction(message, sender, sendResponse) {
 }
 
 browser.runtime.onMessage.addListener(masterFunction);
+
+
+let key = "AIzaSyBiP7J4BatJPBQtLlk8b1yM9STPexFafVI";
